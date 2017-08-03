cde6a596cf5537876a30847eb3e59cb1038f9777
David Carboni <david.carboni@ros.gov.uk>
2017-08-03 14:50:35 +0100

    Updated logging code.

--- logging_standard/__init__.py
+++ logging_standard/__init__.py
@@ -1,28 +1,38 @@
 import os
 from threading import current_thread
 import logging
-from flaskb3 import b3
+from logging import Formatter
+import b3
 
+_log_format = '%(asctime)s %(levelname_spring)+5s %(tracing_information)s' \
+              '%(process_id)s --- [%(thread_name)+15s] %(logger_name)-40s : %(message)s'
 _app_name = None
-_python_record_factory = logging.getLogRecordFactory()
+_python_record_factory = None
 
 
 def init(app, level=None):
     """Initialises logging with the name of the app and wraps the existing record factory."""
+    global _python_record_factory
 
     global _app_name
     _app_name = app.name
 
     # Set up the log format
-    log_format = '%(asctime)s %(levelname_spring)+5s %(tracing_information)s' \
-                 '%(process_id)s --- [%(thread_name)+15s] %(logger_name)-40s : %(message)s'
-    logging.basicConfig(format=log_format, level=level)
+    logging.basicConfig(format=_log_format, level=level)
 
-    # Wrap the existing record factory
-    logging.setLogRecordFactory(_record_factory)
+    if hasattr(logging, "getLogRecordFactory"):
+        # Python 3
+        # getLogRecordFactory was introduced in Python 3
+        _python_record_factory = logging.getLogRecordFactory()
+        logging.setLogRecordFactory(_python3_record_factory)
+    else:
+        # Python 2 fallback
+        # # set the formatter for all handlers on the root logger.
+        for handler in logging.getLogger().handlers:
+            handler.setFormatter(Python2Formatter())
 
 
-def _record_factory(*args, **kwargs):
+def _python3_record_factory(*args, **kwargs):
     """Collates values needed by LOG_FORMAT to implement the logging standard.
 
     :return: A log record augmented with the values required by LOG_FORMAT:
@@ -46,6 +56,26 @@ def _record_factory(*args, **kwargs):
         record.tracing_information = "[" + ",".join(tracing_information) + "] "
 
     return record
+
+
+class Python2Formatter(Formatter):
+    def __init__(self):
+        super(self.__class__, self).__init__(fmt=_log_format)
+
+    def format(self, record):
+        # Standard fields
+        record.levelname_spring = "WARN" if record.levelname == "WARNING" else record.levelname
+        record.process_id = str(os.getpid())
+        record.thread_name = (current_thread().getName())[:15]
+        record.logger_name = record.name[:40]
+        record.tracing_information = ""
+
+        # Optional distributed tracing information
+        tracing_information = _tracing_information()
+        if tracing_information:
+            record.tracing_information = "[" + ",".join(tracing_information) + "] "
+
+        return super(self.__class__, self).format(record)
 
 
 def _tracing_information():
