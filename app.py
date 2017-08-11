62d1639e82b81daa465b4a650a514fc08206d217
David Carboni <david.carboni@ros.gov.uk>
2017-08-11 17:09:21 +0100

    Removed dependency on Flask-Sleuth and deleted that code from this project.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
 import logging
-import sleuth
 import b3
 
 
@@ -11,20 +10,20 @@ app.before_request(b3.start_span)
 app.after_request(b3.end_span)
 
 # Set up logging to display trace information
-logging.getLogger().setLevel(logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
 @app.route("/")
 def home():
-    log.info("Starting")
+    print(logging.getLevelName(log.getEffectiveLevel()))
+    log.warning("Starting with b3 values: {}".format(b3.values()))
 
     with b3.SubSpan() as headers:
         # Pretend to call a downstream service in the sub-span
-        log.info("Calling downstream with headers: {}".format(headers))
+        log.warning("Calling downstream service with b3 values: {}".format(b3.values()))
 
-    log.info("Finishing")
+    log.warning("Finishing with b3 values: {}".format(b3.values()))
 
-    return "Ohai!"
+    return jsonify(b3.values())
 
 app.run(debug=True)
