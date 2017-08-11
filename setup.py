2ac3fe91c66aa54d976a132ce9b0ef8394c7ad05
David Carboni <david.carboni@ros.gov.uk>
2017-08-11 16:54:31 +0100

    Adding pypi deployment.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import unittest
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def test_suite():
+    loader = unittest.TestLoader()
+    suite = loader.discover('tests', pattern='test_*.py')
+    return suite
+
+
+setup(name='Flask-B3',
+      version='0.0.1',
+      description='B3 header access and propagation for Flask.',
+      long_description=readme(),
+      author='David Carboni',
+      author_email='david@carboni.io',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Topic :: System :: Logging',
+          'Topic :: Internet :: Log Analysis',
+          'Intended Audience :: Developers',
+          'Framework :: Flask',
+          'License :: OSI Approved :: MIT License',
+      ],
+      keywords=['logging', 'b3', 'distributed', 'tracing', 'zipkin'],
+      url='https://github.com/davidcarboni/flask_b3',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'flask',
+      ],
+      test_suite='setup.test_suite',
+      include_package_data=True,
+      zip_safe=True,
+      )
