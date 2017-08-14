72825aa82dfa64f654eea0d7d09d5d6808c3bc53
David Carboni <david.carboni@ros.gov.uk>
2017-08-14 14:32:03 +0100

    Demonstrating decorator use, plus some cleanup.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import logging
 import b3
+from b3 import span
 
 
 app = Flask("test")
@@ -14,8 +15,7 @@ log = logging.getLogger(__name__)
 
 
 @app.route("/")
-def home():
-    print(logging.getLevelName(log.getEffectiveLevel()))
+def default():
     log.warning("Starting with b3 values: {}".format(b3.values()))
 
     with b3.SubSpan() as headers:
@@ -24,6 +24,13 @@ def home():
 
     log.warning("Finishing with b3 values: {}".format(b3.values()))
 
+    return jsonify(b3.values())
+
+
+@app.route("/using-decorator")
+@span
+def decorated():
+    log.warning("Using decorator: {}".format(b3.values()))
     return jsonify(b3.values())
 
 app.run(debug=True)
